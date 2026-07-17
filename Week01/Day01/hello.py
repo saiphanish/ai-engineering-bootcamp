30ab7396c58848efb643363faecdf1cb0de5e8e6
saiphanish <saiphanish@gmail.com>
2026-07-17 08:28:45 +0530

    Complete Week 1 Day 1 Python basics

---
+++ Week01/Day01/hello.py
@@ -0,0 +1,26 @@
+""" print("Hello Saiphanish!")
+print("Welcome to AI Engineering Bootcamp")
+name = "phanish"
+age = 45
+experience = 20
+salary = 27.5
+is_lead = True
+city = "hyderabad"
+
+print(f"My name is {name}")
+print(f"I live in {city}")
+print(f"I am {age} years old")
+print(f"I have {experience} years of experience")
+print(f"My target salary is {salary}")
+print(f"This is a lead position: {is_lead}")
+
+currentsalary = 27
+expectedsalary = 40
+difference = expectedsalary - currentsalary
+print(f"My current salary is {currentsalary}")
+print(f"My expected salary is {expectedsalary}")
+print(f"The difference between my expected and current salary is {difference}")  """
+
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+print(type(name))
